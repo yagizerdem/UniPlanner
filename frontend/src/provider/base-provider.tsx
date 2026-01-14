d261df773f4b0bf4434d88a3a47d4fc18500d828
yagizerdem <yagizerdem819@gmail.com>
2026-01-15 02:12:01 +0300

    app and base provider added

---
+++ frontend/src/provider/base-provider.tsx
@@ -0,0 +1,27 @@
+import { createContext, useContext } from "react";
+import { ThemeProvider } from "./theme-provider";
+
+type BaseProviderProps = {
+  children: React.ReactNode;
+};
+
+type BaseProviderState = unknown;
+
+const BaseProviderContext = createContext<BaseProviderState>({});
+
+export function BaseProvider({ children, ...props }: BaseProviderProps) {
+  return (
+    <BaseProviderContext.Provider {...props} value={undefined}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </BaseProviderContext.Provider>
+  );
+}
+
+export const useBase = () => {
+  const context = useContext(BaseProviderContext);
+
+  if (context === undefined)
+    throw new Error("useBase must be used within a BaseProvider");
+
+  return context;
+};
